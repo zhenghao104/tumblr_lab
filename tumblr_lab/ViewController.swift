a6dfad2eb5e18fa5236584a4fbbdb6cbc821209c
Zheng Hao Tang <zht014@ucsd.edu>
2018-02-05 19:34:41 -0800

    Initial Commit

---
+++ tumblr_lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tumblr_lab
+//
+//  Created by Zheng Hao on 2/5/18.
+//  Copyright © 2018 Zheng Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
